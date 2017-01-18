9268142092ced8898189429e848da2a517408ac3
yason4ik <yason4ik87@gmail.com>
2017-01-18 17:26:14 +0200

    add_model_users_and_devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+	
+	def index
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def create
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	def show
+		
+	end
+
+
+end
