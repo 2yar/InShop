5a3e06c8dbbf9433151e638bf3df6a3b1b428cb6
gidpoiiohika <adacer73044210@hmail.com>
2017-01-23 18:30:20 +0200

    add_views_edited_ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,48 @@
 class ProductsController < ApplicationController
 
-	def products_params
- 		params.require(:products).permit(:image_file, :title, :text,)
+
+	def index
+    
+		@products = Product.all
+    end
+
+
+	def new
+        @product = Product.new
+	end 
+
+
+
+	def create
+        @product = Product.new( products_params )
+    end
+
+    
+    def edit
+
+    end
+
+    
+    def update
+
+    end
+    
+
+    	
+    def show
+
+	end
+
+
+	def destroy
+
+	end
+
+
+	private
+	
+    def products_params
+ 		params.require(:products).permit(:price, :name, :description, :image_file) 
     end
 
 end
--- db/migrate/20170121145659_add_paperclip_to_products.rb
+++ db/migrate/20170121145659_add_paperclip_to_products.rb
@@ -1,5 +1,5 @@
 class AddPaperclipToProducts < ActiveRecord::Migration[5.0]
   def change
-  	add_attachment :products, :image
+  	add_attachment :image
   end
 end
