8bb171bea316e2359f2a802acd4169b14ed1856a
gidpoiiohika <adacer73044210@hmail.com>
2017-01-25 21:26:13 +0200

    add_products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,48 +1,74 @@
 class ProductsController < ApplicationController
 
-
-	def index
-    
-		@products = Product.all
+    def index
+        @products = Products.all
     end
 
 
 	def new
-        @product = Product.new
-	end 
+        @product = Products.new
+    end 
 
 
-
-	def create
-        @product = Product.new( products_params )
+    def create
+        @product = Products.new(product_params)
+        if @product.save
+            flash[:notice]="Product was successfully created."
+            redirect_to
+        else
+            render :new
+        end
     end
 
-    
-    def edit
 
+
+    def edit
+        @product = Products.find(params[:id])
+    end
+
+
+    def update
+        @product = Product.current_product
+        if @product.update_attributes(product_params)
+            redirect_to(product_path(@product))
+        else
+            render 'show'
+        end
     end
 
     
     def update
-
+        @product = Products.current_product
+        if @product.update_arrributes(product_params)
+            redirect_to(product_path(@product))
+        else
+            rader 'show'
+        end
     end
-    
+
 
     	
     def show
-
-	end
-
-
-	def destroy
-
-	end
-
-
-	private
-	
-    def products_params
- 		params.require(:products).permit(:price, :name, :description, :image_file) 
+        @product = Products.find(params[:id])
     end
 
+
+    
+    def destroy
+          @product = Products.find(params[:id])
+          @product.destroy
+            respond_to do |format|
+            format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+            format.json { head :no_content }
+        end
+    end
+
+
+
+
+    private
+
+	def product_params
+ 		params.require(:product).permit(:price, :name, :description, :image) 
+    end
 end
--- db/migrate/20170121145659_add_paperclip_to_products.rb
+++ db/migrate/20170121145659_add_paperclip_to_products.rb
@@ -1,5 +1,5 @@
 class AddPaperclipToProducts < ActiveRecord::Migration[5.0]
-  def change
-  	add_attachment :image
-  end
+	def change
+  		add_attachment :image
+  	end
 end
