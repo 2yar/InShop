7a5d4b7dedeb375f38a0467fca5ca734e37640e9
yason4ik <yason4ik87@gmail.com>
2017-01-31 12:16:37 +0200

    errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, styles: {medium: "300x300>", thumb: "100x100>" }, default_url: "default.png"
+  has_attached_file :avatar, styles: { thumb: '100x100>' }, default_url: 'default.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' },
                      controllers: { registrations: "user/registrations" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
