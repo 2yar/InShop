842bf0d80bf0f12a1748958c221cb41232bae9de
yason4ik <yason4ik87@gmail.com>
2017-01-31 12:18:17 +0200

    users_detail_migrate

--- db/migrate/20170118143821_add_details_to_users.rb
+++ db/migrate/20170118143821_add_details_to_users.rb
@@ -1,10 +1,10 @@
 class AddDetailsToUsers < ActiveRecord::Migration[5.0]
   def change
-  	add_column :users, :first_name,  :string,  limit: 	25
-  	add_column :users, :last_name,   :string,  limit: 	50
-  	add_column :users, :phone,       :integer, limit: 	15
-  	add_column :users, :is_admin,    :boolean, default: true
-  	add_column :users, :notes,			 :string,	 limit: 	250
-  	add_column :users, :is_active,	 :boolean, default: true
+    add_column :users, :first_name,  :string
+    add_column :users, :last_name,   :string
+    add_column :users, :phone,       :integer
+    add_column :users, :is_admin,    :boolean, default: true
+    add_column :users, :notes,       :string
+    add_column :users, :is_active,   :boolean, default: true
   end
 end
