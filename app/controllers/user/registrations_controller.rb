c68d31967aa6d6714b72fa0912159ca7a9edec05
yason4ik <yason4ik87@gmail.com>
2017-01-31 13:33:44 +0200

    final_commit

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,7 +1,6 @@
 class User::RegistrationsController < Devise::RegistrationsController
 
   private
-  
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :phone, :notes, :avatar, :email, :password, :password_confirmation, :current_password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@ class User < ApplicationRecord
 
   has_attached_file :avatar, styles: { thumb: '100x100>' }, default_url: 'default.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
 end
