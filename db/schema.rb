5e80615eec27fdc2cb9ea9a688f65346e42c09f6
gidpoiiohika <adacer73044210@hmail.com>
2017-02-18 18:21:38 +0200

    creating cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130124256) do
+ActiveRecord::Schema.define(version: 20170218153418) do
 
   create_table "carts", force: :cascade do |t|
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
-    t.float    "price" default: 0
-    t.float    "weight" 
+    t.float    "price",              default: 0.0
+    t.float    "weight",             default: 0.0
     t.string   "name"
     t.string   "description"
     t.string   "image_file"
     t.integer  "amount",             default: 0
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
